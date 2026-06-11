a552a748bcb071560f0b0766b757822e324be00e
LaVozdeTomelloso <fnavarro@lavozdetomelloso.com>
2026-06-11 18:38:38 +0200

    Adaptar feed para n8n y make inverso

--- api/atom.js
+++ api/atom.js
@@ -71,7 +71,9 @@ module.exports = async (req, res) => {
 
  // ÚLTIMAS 15 NOTICIAS
 for (
-  const item of items.slice(0, 15)
+  const item of items
+    .slice(0, 15)
+    .reverse()
 ) {
 
       try {
